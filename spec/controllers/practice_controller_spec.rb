98e7ab86ad323be89f5b31ba35110f7077afa830
Soumya <soumyajyoti.jh@tamu.edu>
2018-04-29 06:23:51 +0000

    rspec4

--- spec/controllers/practice_controller_spec.rb
+++ spec/controllers/practice_controller_spec.rb
@@ -4,18 +4,19 @@ RSpec.describe PracticeController, type: :controller do
   
   #index
   describe 'questions for practice' do
-    let!(:question1) { FactoryBot.create(:question_bank, question: 'Test Question1', category: 'cat1', option1: '1', option2: '2', option3: '3', option4: '4', option5: '5', answer: '2')}
-    let!(:question2) { FactoryBot.create(:question_bank, question: 'Test Question2', category: 'cat2', option1: '1', option2: '2', option3: '3', option4: '4', option5: '5', answer: '3')}
-    #let(:params) { {categories: {'cat1' => 1}} }
+    let!(:question1) { FactoryBot.create(:question_bank, category: 'cat1', question: 'Test Question1', option1: '1', option2: '2', option3: '3', option4: '4', option5: '5', answer: 2)}
+    let!(:question2) { FactoryBot.create(:question_bank, category: 'cat2', question: 'Test Question2', option1: '1', option2: '2', option3: '3', option4: '4', option5: '5', answer: 3)}
+    let!(:params) { {categories: {'cat1' => 1}} }
     it 'get questions of a given category' do
-      get :index, categories: {'cat1': 1}
+      get :index, categories: {'cat1' => 1}
       #get :index,params[:categories]
       
       # puts params
-      puts question1.question
-      puts params[:categories].keys
-      puts @questions
-      #expect(@questions).to eql(question1.category)
+      # puts question1.question
+      # puts params[:categories].keys
+      puts question1.category
+      puts question2.category
+      expect(assigns(:questions)).to include(question1)
     end
   end
   
