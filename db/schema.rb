02350def93b924c1988953eb0d5779b25576a573
Jonathan Grimes <nsa@tamu.edu>
2018-10-24 04:08:24 +0000

    Setup login functionality finally Merged unique card id into this to do a pull request I had to basically remove all of the previous team\'s User code and rebuild it from scratch

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180402234523) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+ActiveRecord::Schema.define(version: 20181024034452) do
 
   create_table "question_banks", force: :cascade do |t|
     t.string   "category"
@@ -32,11 +29,11 @@ ActiveRecord::Schema.define(version: 20180402234523) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
+    t.string   "password_digest"
+    t.string   "current_place"
+    t.boolean  "is_admin"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "password_digest"
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
