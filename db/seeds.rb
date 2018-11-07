6079be30c778530cbd65de1dcd2a7b31edaf4313
Jonathan Aruchichichutio <jonathanearauco@gmail.com>
2018-11-07 04:49:21 +0000

    added image

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-
 question_banks = [
-{	
+	{	
 	:category => 'Networks',
 		:question => 'In the Internet Protocol (IP) suite of protocols, which of the following best describes the purpose of the Address Resolution Protocol?',
 		:option1 => 'To translate Web addresses to host names',
 		:option2 => 'To determine the IP address of a given host name',
 		:option3 => 'To determine the hardware address of a given host name',
 		:option4 => 'To determine the hardware address of a given IP address',
-		:option5 => 'To determine the appropriate route for a datagram', :answer => 2},
+		:option5 => 'To determine the appropriate route for a datagram',
+		:answer => 2
+	},
 	{	:category => 'Networks', 
 		:question => 'testImage.png', 
 		:option1 => 'Test Answer',
 		:option2 => 'Test Answer',
 		:option3 => 'Test Answer',
 		:option4 => 'Test Answer',
-		:option5 => 'Test Answer', :answer => 2},
-{	:category => 'Operating Systems',
+		:option5 => 'Test Answer', 
+		:answer => 2
+	},
+	{	
+		:category => 'Operating Systems',
 		:question => 'The real disadvantage of a linear list of directory entries is the :',
 		:option1 => 'Size of the linear list in memory',
 		:option2 => 'Linear search to find a file',
