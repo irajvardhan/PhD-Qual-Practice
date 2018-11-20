7331a171c83902c62f5a6be7d829ac0b0f191a95
Jonathan Grimes <nsa@tamu.edu>
2018-11-20 02:47:59 +0000

    Minor Fixes to migration files

--- db/migrate/20181113060748_add_creator_review_status_explaination_to_question_bank.rb
+++ db/migrate/20181113060748_add_creator_review_status_explaination_to_question_bank.rb
@@ -1,7 +1,6 @@
 class AddCreatorReviewStatusExplainationToQuestionBank < ActiveRecord::Migration
   def change
     add_column :question_banks, :creator, :string
-    add_column :question_banks, :reviewStatus, :string
     add_column :question_banks, :explaination, :string
   end
 end
