6e55914f00b377c4e6e70a6b80e538f79d79ab5d
Anudeep Balusani <sonuanudeep123@gmail.com>
2019-03-19 17:26:26 -0500

    test nav buttons admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,7 +2,7 @@ class AdminController < ApplicationController
   before_action :logged_in_admin, only: [:index, :show]
   
   def index
-    @questions = QuestionBank.all
+    #@questions = QuestionBank.all
     @users = User.all
   end
   
