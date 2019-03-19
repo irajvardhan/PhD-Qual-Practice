d5581dbd22b8c2f61ce3a8b7a59b08032a10c20f
Anudeep Balusani <sonuanudeep123@gmail.com>
2019-03-19 18:32:28 -0500

    nav buttons admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
   resources :admin
-  get '/admin' =>  'admin#index'
-  get '/admin_questions' => 'admin#admin_questions' 
-  get '/admin/:id' => 'admin#show'
+  get '/admin', to:  'admin#index'
+  get '/admin_questions', to: 'admin#admin_questions' 
+  get '/admin/:id', to: 'admin#show'
   
   resources :users
   get '/register', to: 'users#new'
