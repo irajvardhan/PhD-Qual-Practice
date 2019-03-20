5ee26320db870975c2ea7131402eab8a04f6f213
Anudeep Balusani <sonuanudeep123@gmail.com>
2019-03-20 12:32:34 -0500

    test users delete

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,13 +2,11 @@ class AdminController < ApplicationController
   before_action :logged_in_admin, only: [:index, :show]
   
   def index
-    @questions = QuestionBank.all
     @users = User.all
   end
   
   def admin_questions
     @questions = QuestionBank.all
-    @users = User.all
   end
 
   def show
@@ -17,4 +15,12 @@ class AdminController < ApplicationController
     redirect_to action: "index"
     return
   end
+
+  def destroy
+    @question = QuestionBank.find(params[:id])
+    @question.destroy
+    flash[:destroy] = "Question-'#{@question.id}' deleted."
+    redirect_to admin_questions_path
+  end
+
 end
