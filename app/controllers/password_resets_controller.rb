afdc9a938762cefa5f7b9713b573925f8f0e9695
EC2 Default User <ec2-user@ip-172-31-17-151.us-east-2.compute.internal>
2019-03-24 21:17:59 +0000

    Modified flash error for create method for password_reset controller

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -11,7 +11,7 @@ class PasswordResetsController < ApplicationController
       redirect_to root_url
     else
       flash.now[:notice] = "Email address not found"
-      render new_password_reset_path
+      redirect_to new_password_reset_path
     end
   end
   
