7929027df96748bb5e84c4e6524d1919239666c9
EC2 Default User <ec2-user@ip-172-31-17-151.us-east-2.compute.internal>
2019-03-25 16:15:16 +0000

    Added User mailer functionality- v1

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -8,7 +8,7 @@ class PasswordResetsController < ApplicationController
       #@user.create_reset_digest
       #@user.send_password_reset_email
       flash[:info] = "Email sent with password reset instructions"
-      redirect_to root_url
+      redirect_to login_path
     else
       flash[:notice] = "Email address not found"
       redirect_to login_path
