f7ec25258b75006e735acdd86b5fb1f14a35fade
EC2 Default User <ec2-user@ip-172-31-17-151.us-east-2.compute.internal>
2019-03-25 17:35:17 +0000

    Added User mailer functionality- v1

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,13 @@
 class UserMailer < ApplicationMailer
 
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.password_reset.subject
+  #
   def password_reset
-   # @greeting = "Hi"
+    @greeting = "Hi"
 
-    #mail to: "sudarshan@tamu.edu"
+    mail to: "sudarshan@tamu.edu"
   end
 end
