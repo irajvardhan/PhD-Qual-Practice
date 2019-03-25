8a1d057373e7a0bbe2b3636997ab46739023633a
EC2 Default User <ec2-user@ip-172-31-17-151.us-east-2.compute.internal>
2019-03-25 20:51:51 +0000

    Added User mailer functionality- v1

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,8 @@
 class UserMailer < ApplicationMailer
 
- def password_reset
-    #@user = user
-    #mail(to: @user.email, subject: 'Sample Email').deliver_now
-    mail(to:"sudarshan@tamu.edu", subject: 'test', body: "body text for mail")
+ def password_reset(user)
+    mail(to: user.email, subject: 'Sample Email').deliver_now
+    #mail(to:"sudarshan@tamu.edu", subject: 'test', body: "body text for mail")
  end
    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -20,8 +20,7 @@ class User < ActiveRecord::Base
   
   # Sends password reset email.
   def send_password_reset_email
-     message = UserMailer.password_reset
-    message.deliver_now
+    UserMailer.password_reset(self).deliver
     #.deliver_now
   end
 
