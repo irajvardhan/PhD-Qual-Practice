e6e966729684b4e3a900a971a2535b0b71cd7772
EC2 Default User <ec2-user@ip-172-31-17-151.us-east-2.compute.internal>
2019-03-25 23:59:41 +0000

    Added User mailer functionality- v3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'https://enigmatic-wildwood-12647.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
