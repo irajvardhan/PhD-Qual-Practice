6a4b38bf0304d6f96ebc52d36d15e51adfb0b88f
EC2 Default User <ec2-user@ip-172-31-17-151.us-east-2.compute.internal>
2019-03-26 13:44:51 +0000

    Added User mailer functionality-V6

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,15 @@
 class User < ActiveRecord::Base
     has_secure_password
     attr_accessor :reset_token
-    
     before_save   :downcase_email
 
   # Sets the password reset attributes.
   def create_reset_digest
     self.reset_token= SecureRandom.urlsafe_base64
     #reset_digest=User.digest(reset_token)
-    #reset_sent_at=Time.zone.now
-    update_columns(reset_digest:  User.digest(reset_token), reset_sent_at: Time.zone.now)
+    self.reset_sent_at=Time.zone.now
+    save!
+   # update_columns(reset_digest:  User.digest(reset_token), reset_sent_at: Time.zone.now)
   end
   
   def User.digest(string)
@@ -22,7 +22,7 @@ class User < ActiveRecord::Base
   def send_password_reset_email
     UserMailer.password_reset(self).deliver
   end
-
+  
   # Returns true if a password reset has expired.
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
