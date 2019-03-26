150a5d8473ed7308044e72fad0239de971a1504e
EC2 Default User <ec2-user@ip-172-31-17-151.us-east-2.compute.internal>
2019-03-26 00:02:03 +0000

    Added User mailer functionality- v3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,18 +15,19 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://enigmatic-wildwood-12647.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { :host => 'https://enigmatic-wildwood-12647.herokuapp.com' }
+  
   config.action_mailer.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
-  }
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => "gmail.com",
+  :user_name            => "bisibele.techies@gmail.com",
+  :password             => "Bisibele2017",
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+}
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
