47b7d55ec20a662b960d8228e7c8478a8546cfb6
EC2 Default User <ec2-user@ip-172-31-17-151.us-east-2.compute.internal>
2019-03-26 04:28:48 +0000

    Added User mailer functionality- v4

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -20,21 +20,18 @@ class PasswordResetsController < ApplicationController
   end
   
     def update
-    if params[:user][:password].empty?                  # Case (3)
-      @user.errors.add(:password, "can't be empty")
-      render 'edit'
-    elsif @user.update_attributes(user_params)          # Case (4)
-      log_in @user
-      @user.update_attribute(:reset_digest, nil)
-      flash[:success] = "Password has been reset."
-      redirect_to @user
-    else
-      render 'edit'                                     # Case (2)
+      @user = User.find_by_password_reset_token!(params[:id])
+      if @user.password_reset_sent_at < 2.hours.ago
+        redirect_to new_password_reset_path, :alert => "Password reset has expired."
+      elsif @user.update_attributes(params[:user])
+        redirect_to root_url, :notice => "Password has been reset!"
+      else
+        render :edit
+      end
     end
-  end
   
   def edit
-    redirect_to edit_password_reset_path(User.find_by_email(user.email)[:id])
+   @user = User.find_by_password_reset_token!(params[:id])
   end
   
    private
