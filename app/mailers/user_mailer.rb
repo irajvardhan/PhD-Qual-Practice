c22f9ab36026ea9841ddcf87bba959b54aa51ad8
EC2 Default User <ec2-user@ip-172-31-17-151.us-east-2.compute.internal>
2019-03-26 00:26:37 +0000

    Added User mailer functionality- v3

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
 
  def password_reset(user)
-    mail to: user.email, subject: 'Password Reset for PhD Qual Practice App', body: "Reset Link: #{edit_password_reset_path(User.find_by_email(user.email)[:id])}"
+    mail to: user.email, subject: 'Password Reset for PhD Qual Practice App', body: "Reset Link: #{edit_password_reset_url(User.find_by_email(user.email)[:id])}"
  end
    
 end
