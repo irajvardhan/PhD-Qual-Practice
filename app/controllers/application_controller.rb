b8fc1429411aa35be9baeb17fd7fd9e54d7b3779
EC2 Default User <ec2-user@ip-172-31-43-230.us-east-2.compute.internal>
2019-04-01 23:54:38 +0000

    added mobile phone detection in application_controller;created new css for mobile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,36 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+  
+  helper :all
+  protect_from_forgery
+  before_filter :prepare_for_mobile
 
+  layout :which_layout
+  def which_layout
+    mobile_device? ? 'mobile' : 'application'
+  end
+  
   protected
   def authorize
     unless User.find_by(id: session[:user_id])
       redirect_to login_url, notice:"You need to login to see this page"
     end
+  end
+
+  private
+  
+  def mobile_device?
+    if session[:mobile_param]
+      session[:mobile_param] == "1"
+    else
+      request.user_agent =~ /Mobile|webOS/
+    end
+  end
+  helper_method :mobile_device?
+  
+  def prepare_for_mobile
+    session[:mobile_param] = params[:mobile] if params[:mobile]
+    request.format = :mobile if mobile_device?
   end
 end
