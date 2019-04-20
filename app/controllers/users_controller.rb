89f77e90b66220325f7a078136041d830d5ba1c4
EC2 Default User <ec2-user@ip-172-31-17-151.us-east-2.compute.internal>
2019-04-20 18:28:52 +0000

    Merged iterations 1, 2, 3 --fixed password reset url

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_admin, only: [:index, :show, :edit, :update, :destroy]
+  before_action :logged_in_admin, only: [:index, :edit, :update, :destroy] #, :show
   
 
   # GET /users
