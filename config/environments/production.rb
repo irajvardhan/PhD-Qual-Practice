d4a25b4dbece2168e1a99f2e711c509f8b2912b3
farabi <smfmj01@yahoo.com>
2020-04-01 18:35:13 -0500

    corrected production configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,14 +17,14 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'https://pure-lowlands-76347.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'https://strawberry-cake-09482.herokuapp.com/' }
   
   config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "gmail.com",
-  :user_name            => "bisibele.techies@gmail.com",
-  :password             => "Bisibele2017",
+  :user_name            => ENV['GMAIL_USERNAME'],
+  :password             => ENV['GMAIL_PASSWORD'],
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
